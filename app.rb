bd1f138c30acfd661cad28679063cdf9f704ef6d
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-08 21:06:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,14 +26,11 @@ class App < Sinatra::Base
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     words = []
 
-    "hello"
-
-    words < params[:word1]
-    words < params[:word2]
-    words < params[:word3]
-    words < params[:word4]
-    words < params[:word5]
-    binding.pry
+    words << params[:word1]
+    words << params[:word2]
+    words << params[:word3]
+    words << params[:word4]
+    words << params[:word5]
 
   end
 
