4a417eed72490fe4d8b39b711e1c179b945fc8bb
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-08 21:16:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -37,7 +37,19 @@ class App < Sinatra::Base
   end
 
   get "/:operation/:number1/:number2" do
-    
+    @operation = params[:operation]
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+
+    if @operation == "add"
+      "#{@num1+@num2}"
+    elsif @operation == "subtract"
+      "#{@num1-@num2}"
+    elsif @operation == "multiply"
+      "#{@num1*@num2}"
+    else @operation == "divide"
+      "#{@num1/@num2}"
+    end      
   end
 
 
